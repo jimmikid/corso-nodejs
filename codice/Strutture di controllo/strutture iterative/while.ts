43180b74fb593253b2d4a429488af493f5332587
jimmikid <gianmarco.stinchi@hotmail.com>
2020-09-30 13:53:06 +0200

    Lezione su strutture di controllo

---
+++ codice/Strutture di controllo/strutture iterative/while.ts
@@ -0,0 +1,19 @@
+/**
+ * 
+ * IL CICLO WHILE
+ * 
+ */
+
+let i: number = 1;
+
+while (i <= 50) {
+
+    //Blocco di istruzioni
+
+    console.log(i);
+    
+    //Incremento i
+    i++;
+}
+
+console.log("continuo col programma...\n\n\n\n");
