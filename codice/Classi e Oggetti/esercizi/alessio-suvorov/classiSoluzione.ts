2524e94200f4af0f486c0e8ea22338aa69d86687
Oleksii Suvorov <osuvorov@eagleprojects.it>
2020-10-19 11:23:42 +0200

    add some corrections to formulas

--- codice/Classi e Oggetti/esercizi/alessio-suvorov/classiSoluzione.ts
+++ codice/Classi e Oggetti/esercizi/alessio-suvorov/classiSoluzione.ts
@@ -1,12 +1,11 @@
 // Creare una classe che rappresenta un punto del piano con coordinate ( x,y ) con relativi getters e setters
-
 class Point {
     constructor(private _x, private _y) {
         if (Number(_x) && Number(_y)) {
             this._x = _x
             this._y =_y
         } else {
-            throw new TypeError("Properties should be of type number.");
+            throw new TypeError("Properties should be of type number.")
         }
     }
     
@@ -27,16 +26,16 @@ class Point {
     }
 
     public calculateDistance(point2: Point): number {
-        return Math.sqrt( Math.pow( (point2.x - this.x) , 2) + Math.pow((point2.y - this.y), 2) )
+        return Math.sqrt(Math.pow( (point2.x - this.x) , 2) + Math.pow((point2.y - this.y), 2) )
     }
 }
 
-let point1 = new Point(1, 2);
-let point2 = new Point(2, 3);
-console.log(point1.calculateDistance(point2));
+console.log("==================Exercise_1========================")
+let point1 = new Point(1, 2)
+let point2 = new Point(2, 3)
+console.log(`Distance between two points: point1(${point1.x}, ${point1.y}) and point2(${point2.x}, ${point2.y}) is ${point1.calculateDistance(point2)}`)
 
 //Creare una classe che rappresenta un Triangolo che prende in ingresso la lunghezza dei tre lati con relativi getters e setters
-
 class Triangle {
     private readonly EQUILATERAL = 1
     private readonly SCALENE = 2
@@ -44,16 +43,16 @@ class Triangle {
     private base: number;
     private typetreangle: number = null
     constructor(private _part1: number, private _part2: number, private _part3: number) {
-        if(Number(_part1) && Number(_part1) && Number(_part1)) {
+        if(Number(_part1) && Number(_part2) && Number(_part3)) {
             this._part1 = _part1
             this._part2 = _part2
             this._part3 = _part3
-        } else if(this.controllTriangleLength()) {
-            throw new Error("Dead triangle =)")
+            if(this.controllTriangleLength()) {
+                throw new Error("Dead triangle =)")
+            } 
         } else {
             throw new TypeError("Properties should be of type number.")
         }
-
     }
     private controllTriangleLength(): boolean {
         if(this._part1 > (this._part2 + this._part3) || this._part2 > (this._part1 + this._part3) || this._part3 > (this._part1 + this._part2) ) {
@@ -65,13 +64,13 @@ class Triangle {
     public checkRectangleType(): void {
         if(this._part1 === this._part2 && this._part2 === this._part3 ) {
             this.typetreangle = this.EQUILATERAL
-            console.log("Treangle is equilateral");
+            console.log("Treangle is equilateral")
         } else if (this._part1 !== this._part2 && this._part2 !== this._part3) {
             this.typetreangle = this.SCALENE
-            console.log("Treangle is scalene");
+            console.log("Treangle is scalene")
         } else {
             this.typetreangle = this.ISOSCELES
-            console.log("Treangle is isosceles");
+            console.log("Treangle is isosceles")
         }
     }
 
@@ -88,6 +87,7 @@ class Triangle {
             this.base = 3
         }
         if (base) {
+            console.log(`Base is ${base}`)
            return base 
         } else {
             throw new Error("Not an isosceles treangle")
@@ -98,29 +98,39 @@ class Triangle {
         if(this.typetreangle === null) {
             this.checkRectangleType()
         } 
+        
         if(this.typetreangle === this.EQUILATERAL) {
             let altezza = this._part1 * Math.sqrt(3) / 2
-            console.log(`Rectangle is equilateral. Area: ${Math.pow(this._part1, 2) * Math.sqrt(3) / 4}`);
-        } else if(this.typetreangle === this.SCALENE) {
-            console.log(`Rectangle is scalene. Area: ${0.5 * this._part1 * this._part2 * Math.sin(this._part1)}`);
+            console.log(`Rectangle is equilateral. Area: ${Math.pow(this._part1, 2) * Math.sqrt(3) / 4}`)
+        } else if(this.typetreangle === this.SCALENE) {   
+            let p = 0.5 * (this._part1 + this._part2 + this._part3)
+            console.log(`Rectangle is scalene. Area: ${Math.sqrt(p * (p-this._part1) * (p - this._part2) * (p-this._part3))}`)
         } else {
             let base = this.findBase();
+            let altezza;
             if (this.base === 1) {
-                console.log(`Rectangle is isosceles. Area: ${0.5 * Math.pow(this._part2, 2) * Math.sin(this._part1)}`);
+                altezza = Math.sqrt(Math.pow(this._part2, 2) - Math.pow(base, 2) / 4)
+                console.log(`Rectangle is isosceles. Area: ${0.5 * base * altezza}`)
             } else if (this.base === 2) {
-                console.log(`Rectangle is isosceles. Area: ${0.5 * Math.pow(this._part1, 2) * Math.sin(this._part2)}`);
+                altezza = Math.sqrt(Math.pow(this._part1, 2) - Math.pow(base, 2) / 4)
+                console.log(`Rectangle is isosceles. Area: ${0.5 * base * altezza}`)
             } else {
-                console.log(`Rectangle is isosceles. Area: ${0.5 * Math.pow(this._part1, 2) * Math.sin(this._part3)}`);
+                altezza = Math.sqrt(Math.pow(this._part2, 2) - Math.pow(base, 2) / 4)
+                console.log(`Rectangle is isosceles. Area: ${0.5 * base * altezza}`)
             }
         }
         
     }
 }
-
+console.log("==================Exercise_2========================")
 let triangleEquilateral = new Triangle(3, 3, 3)
-let triangleScalene = new Triangle(3, 2, 1)
-let triangleIsosceles = new Triangle(3, 3, 8)
+let triangleScalene = new Triangle(6, 4, 3)
+let triangleIsosceles = new Triangle(6, 6, 3)
 
 triangleEquilateral.checkRectangleType()
 triangleScalene.checkRectangleType()
 triangleIsosceles.checkRectangleType()
+
+triangleEquilateral.calculateArea()
+triangleScalene.calculateArea()
+triangleIsosceles.calculateArea()
